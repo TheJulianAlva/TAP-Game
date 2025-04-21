fb8f0d8293aae06c9840c1fafebf1d1be7ce635f
JulianchoAlva <josejulianalvaz@gmail.com>
2025-04-20 20:22:31 -0600

    Se eliminan elementos duplicados y se añade capa de colision a habitaciones

--- camera_2d2.gd
+++
@@ -1,7 +0,0 @@
-extends Camera2D
-
-#conecta la posicion de la camara con el cuarto que manda EVENTS
-func _ready() -> void:
-	EVENTS.room_entered.connect(func(room):
-		global_position = room.global_position 
-		)
--- source/screens/room/room.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-#cuando ingresa a un cuarto emite la señal del cuarto donde se dectectó
-func _on_player_detector_body_entered(body: Node2D) -> void:	
-	EVENTS.room_entered.emit(self)
-	print (self.name)
