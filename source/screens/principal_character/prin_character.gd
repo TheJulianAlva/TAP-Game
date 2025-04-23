ae324cbbd2063b4abd7dfd6899cfff738131779d
JulianchoAlva <josejulianalvaz@gmail.com>
2025-04-22 21:51:34 -0600

    Se añade animacion de daño y control de colisiones con enemigos a Player

--- source/screens/principal_character/prin_character.gd
+++ source/screens/principal_character/prin_character.gd
@@ -10,19 +10,36 @@ class_name Player extends CharacterBody2D
 
 
 @onready var anim_player: AnimatedSprite2D = $AnimatedSprite
+@onready var effects: AnimationPlayer = $Effects
+@onready var hurt_timer: Timer = $HurtTimer
+
+
 #var speed: int = GLOBAL.pChar_speed #Comentado mientras se hacen modificaciones a pantallas específicas
 var speed = 70
 @export var maxHealth:int = 5
 @onready var currentHealth:int = 5
 signal healthChanged
 
+var knockBackPower:float = 20.0
+
 var currentDirection: String = "down"
+
+var isHurt:bool = false
+var enemyCollisions = []
+
+func _ready() -> void:
+	effects.play("RESET")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
 	update_velocity()
 	move_and_slide()
 	handle_animation()
+	if !isHurt:
+		for enemyArea in enemyCollisions:
+			hurt_by_enemy()
+	
+
 
 func handle_animation() -> void:
 	if velocity.length() == 0:
@@ -47,9 +64,28 @@ func update_velocity() -> void:
 	var direction:Vector2 = Input.get_vector("WASD_A", "WASD_D", "WASD_W", "WASD_S")
 	velocity = direction * speed
 
+func hurt_by_enemy() -> void:
+	currentHealth -= 1
+	if currentHealth < 0: #Asignacion máxima temporal
+		currentHealth = maxHealth
+	healthChanged.emit(currentHealth)
+	isHurt = true
+	knock_back()
+	effects.play("hurtAnimation")
+	hurt_timer.start()
+	await hurt_timer.timeout
+	effects.play("RESET")
+	isHurt = false
+
 func _on_hit_box_area_entered(area: Area2D) -> void:
+	if isHurt: return
 	if area.name == "HitBox":
-		currentHealth -= 1
-		if currentHealth < 0: #Asignacion máxima temporal
-			currentHealth = maxHealth
-		healthChanged.emit(currentHealth)
+		enemyCollisions.append(area)
+		
+
+func knock_back():
+	velocity = -velocity * knockBackPower
+	move_and_slide()
+
+func _on_hit_box_area_exited(area: Area2D) -> void:
+	enemyCollisions.erase(area)
