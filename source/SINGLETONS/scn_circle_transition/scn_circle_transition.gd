e270a4d08475713116205ebf5185eef22be2d7d4
Juliancho <josejulianalvaz@gmail.com>
2025-05-03 15:31:51 -0600

    Feat/incorporar UI-Theme a pantallas (#15)
    * refactor: restructurar containers en Configuracion #14 y agregar UI-Theme
    
    * refactor: #14 agregar UI-Theme a PauseScreen

--- source/SINGLETONS/scn_circle_transition/scn_circle_transition.gd
+++ source/SINGLETONS/scn_circle_transition/scn_circle_transition.gd
@@ -7,10 +7,12 @@ func _ready() -> void:
 	layer = -5
 
 func change_scene(path:String) -> void:
-	layer = 1
+	layer = 5
+	get_tree().paused = true
 	an_fade.play("circle_fade")
 	await(an_fade.animation_finished)
 	get_tree().change_scene_to_file(path)
 	an_fade.play_backwards("circle_fade")
 	await(an_fade.animation_finished)
-	layer = -1
+	layer = -5
+	get_tree().paused = false
--- source/screens/NON_GAME_SCREENS/screen_intro/screen_intro.gd
+++ source/screens/NON_GAME_SCREENS/screen_intro/screen_intro.gd
@@ -1,6 +1,10 @@
-extends Node
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$BtnAMenuPrin.grab_focus()
+	$MarginContainer/btnSkip.grab_focus()
+
+
+func _on_btn_skip_pressed() -> void:
+	SCN_CIRCLE_TRANSITION.change_scene(GLOBAL.SCREEN_PRINCIPAL_PATH)
