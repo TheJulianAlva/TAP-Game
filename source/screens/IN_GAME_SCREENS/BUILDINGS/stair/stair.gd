f88c65c9268be95bbe9af379cf7c919a3e62407a
Ledesma3110 <cledesmag.3110@gmail.com>
2025-05-07 08:32:55 -0600

    feat: #17 crear stair

--- source/screens/IN_GAME_SCREENS/BUILDINGS/stair/stair.gd
+++ source/screens/IN_GAME_SCREENS/BUILDINGS/stair/stair.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
-@onready var spawn_location_S: Marker2D = $SpawnLocationS
+@onready var spawn_location_stair: Marker2D = $SpawnLocationStair
 
 func _on_body_entered(body: Node2D) -> void:
-	body.global_position = spawn_location_S.global_position
+	print("Collision")
+	body.global_position = spawn_location_stair.global_position
