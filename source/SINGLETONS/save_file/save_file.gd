297559b1573de7bfd09a9529b4e37e10dc1272ee
Juliancho <josejulianalvaz@gmail.com>
2025-05-14 08:29:48 -0600

    Crear file save (#26)
    * #24 crear file_save y file_load
    
    * #24 crear save_file
    
    * #24 modificar save file
    
    * #24 modificar bien hecho save file
    
    ---------
    
    Co-authored-by: Ledesma3110 <cledesmag.3110@gmail.com>

--- source/SINGLETONS/save_file/save_file.gd
+++ source/SINGLETONS/save_file/save_file.gd
@@ -1,25 +1,31 @@
 extends Node
 
-func save_file():
-	var datos={
-		"pChar_max_healt": GLOBAL.pChar_max_health,
-		"pChar_current_healt": GLOBAL.pChar_current_health,
-		"pChar_base_damage": GLOBAL.pChar_base_damage,
-		"player_has_lamp": GLOBAL.player_has_lamp
-	}
-	var jsonString = JSON.stringify(datos)
-	var jsonFile = FileAccess.open("res://gamesaved.json",FileAccess.WRITE)
-	jsonFile.store_line(jsonString)
-	jsonFile.close()
-	print("Saved")
-	print("chk")	
-func load():
-	var jsonFile = FileAccess.open("res://gamesaved.json",FileAccess.READ)
+func save_game():
+		var datos = {
+			"pChar_max_healt": GLOBAL.pChar_max_health,
+		  "pChar_current_healt": GLOBAL.pChar_current_health,
+		  "pChar_base_damage": GLOBAL.pChar_base_damage,
+		  "player_has_lamp": GLOBAL.player_has_lamp
+		}
+		var jsonString = JSON.stringify(datos)
+		var jsonFile = FileAccess.open("res://gamesaved.json", FileAccess.WRITE)
+		if jsonFile:
+			jsonFile.store_line(jsonString)
+			jsonFile.close()
+			print("DATA FILE SAVED...")
+		else:
+			print("ERROR: CANNOT FIND FILE SAVE...")
+
+
+func load_game():
+	var jsonFile = FileAccess.open("res://gamesaved.json", FileAccess.READ)
 	var jsonString = jsonFile.get_as_text()
 	jsonFile.close()
+	
 	var datos = JSON.parse_string(jsonString)
-	GLOBAL.pChar_max_health = datos.pChar_max_healt
-	GLOBAL.pChar_current_health = datos.pChar_current_healt
+	GLOBAL.pChar_max_health = datos.pChar_max_health
+	GLOBAL.pChar_current_health = datos.pChar_current_health
 	GLOBAL.pChar_base_damage = datos.pChar_base_damage
-	GLOBAL.player_has_lamp = datos.player_has_lamp
-	print("Loaded")
+	GLOBAL.pChar_high_heal = datos.pChar_high_heal
+	print("DATA FILE LOADED...")
+	
