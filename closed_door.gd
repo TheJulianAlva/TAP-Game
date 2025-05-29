f4d7c554bdda474cc26b3d80b578649eee9cf01b
Ledesma3110 <cledesmag.3110@gmail.com>
2025-05-28 23:46:39 -0600

    #11 agregar label

--- closed_door.gd
+++ closed_door.gd
@@ -2,3 +2,11 @@ extends StaticBody2D
 
 func _on_key_door_opened() -> void:
 	$Sprite2D.visible = false
+	$CollisionShape2D.queue_free()
+
+func _on_area_2d_door_body_entered(body: Node2D) -> void:	
+	if body is Player:
+		$LabelConfirm.visible = true
+
+func _on_area_2d_door_body_exited(body: Node2D) -> void:
+	$LabelConfirm.visible = false
