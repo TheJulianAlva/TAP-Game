c93ed87a62f1f6f58d4099e51e92d46a88e8b5b1
Ledesma3110 <cledesmag.3110@gmail.com>
2025-05-28 19:06:06 -0600

    #11 crear la llave que desaparece la puerta

---
+++ closed_door.gd
@@ -0,0 +1,4 @@
+extends StaticBody2D
+
+func _on_key_door_opened() -> void:
+	$Sprite2D.visible = false
---
+++ source/screens/IN_GAME_SCREENS/TOOLS/key/key.gd
@@ -0,0 +1,31 @@
+extends StaticBody2D
+
+signal door_opened
+
+var keytaken = false
+var in_door_zone = false
+
+#replace node2D to PhysicsBody2D
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Player:
+		if keytaken == false:
+			print("into")
+			keytaken = true
+			$Sprite2D.queue_free()
+
+func _process(delta: float) -> void:
+	if keytaken == true:
+		if in_door_zone == true:
+			if Input.is_action_just_pressed("ui_accept"):
+				print("door_opened")
+				emit_signal("door_opened")
+
+func _on_area_2d_door_body_entered(body: Node2D) -> void:
+	if body is Player:
+		in_door_zone = true
+		print(in_door_zone)
+
+func _on_area_2d_door_body_exited(body: Node2D) -> void:
+	if body is Player:
+		in_door_zone = false
+		print(in_door_zone)
