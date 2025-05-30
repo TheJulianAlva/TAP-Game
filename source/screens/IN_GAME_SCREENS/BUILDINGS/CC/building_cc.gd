a6d9a09dbdbfd319bf451240307d69fa37ae4faf
Ledesma3110 <cledesmag.3110@gmail.com>
2025-05-30 07:25:41 -0600

    Crear TouchScreen (#37)
    * #32 abrir markers en el mapa
    
    * añadir policia
    
    * *modified principal_character
    
    * ingresar el policia en la pantalla de juego
    
    * #32 agregar AccessDoor en edificios para regresar a MenuPrincipal
    
    ---------
    
    Co-authored-by: JulianchoAlva <josejulianalvaz@gmail.com>

---
+++ source/screens/IN_GAME_SCREENS/BUILDINGS/CC/building_cc.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+func _ready() -> void:
+	$Player.hasLamp = GLOBAL.player_has_lamp
+
+func _on_access_door_scene_change() -> void:
+	GLOBAL.current_mark = GLOBAL.Markers.mk_building_cc
