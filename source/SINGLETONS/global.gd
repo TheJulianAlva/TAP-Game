2550a10073022061e31b7e7621dbe79adffad3c9
JulianchoAlva <josejulianalvaz@gmail.com>
2025-05-29 21:26:25 -0600

    #10 conectar buses con controles

--- source/SINGLETONS/global.gd
+++ source/SINGLETONS/global.gd
@@ -9,6 +9,17 @@ var SCREEN_CONTROLS_PATH:String = "res://source/SCREENS/NON_GAME_SCREENS/screen_
 #Declaraciones de rutas de pantallas jugables
 var SCREEN_GAME_WORLD_PATH:String = "res://source/SCREENS/IN_GAME_SCREENS/screen_game_world/screen_game_world.tscn"
 
+#Declaraciones de Buses
+@onready var bus_master_id=AudioServer.get_bus_index("Master")
+@onready var bus_sfx_id=AudioServer.get_bus_index("SFX")
+@onready var bus_voice_id=AudioServer.get_bus_index("Voice")
+@onready var bus_music_id=AudioServer.get_bus_index("Music")
+
+var volume_master:float		##volumen maestro
+var volume_music:float		##volumen de la música
+var volume_sfx:float		##volumen de los efectos de sonido
+var volume_voice:float		##volumen de las voces
+
 var player_has_lamp:bool = true
 
 #Declaracion de marcadores para ubicacion de personaje
@@ -40,7 +51,24 @@ func _ready() -> void:
 	pChar_low_heal = character_settings.lowheal
 	pChar_high_heal = character_settings.highheal
 	pChar_name = character_settings.name
-	#asignación de valores Enemy
 	
+	#asignación de valores Enemy
 	var enemy_setting=CONFIG_FILE.load_enemy_setting()
 	enemy_speed = enemy_setting.speed
+	
+	#asignación de valores Audio
+	var audio_settings = CONFIG_FILE.load_audio_setting()
+	volume_master = audio_settings.volume_master
+	set_bus_volume(bus_master_id, volume_master)
+	volume_sfx = audio_settings.volume_sfx
+	set_bus_volume(bus_sfx_id, volume_sfx)
+	volume_music = audio_settings.volume_music
+	set_bus_volume(bus_music_id, volume_music)
+	volume_voice = audio_settings.volume_voice
+	set_bus_volume(bus_voice_id, volume_voice)
+	
+	
+
+func set_bus_volume(bus,volume):
+	AudioServer.set_bus_volume_db(bus,linear_to_db(volume))
+	AudioServer.set_bus_mute(bus,volume<0.05)
